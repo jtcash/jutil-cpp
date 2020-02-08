5091da70437ae3949973b76fc9f2a3e742a6bce2
Jeffrey Cash <jtcash@ucsd.edu>
2020-02-07 19:12:47 -0800

    updated info

--- jutil.hpp
+++ jutil.hpp
@@ -4,9 +4,13 @@
  *  A set of header files to provide useful tools to aid in development and debugging.
  *  jutil is expected to rely heavily on C++17 features, and targets GCC, though MSVC support is
  * expected for most features.
- * 
+ *  Everything, except for the occasional macro definition when unavoidable, is contained in
+ * the `jeff` namespace.
  **/
 
+//// NOTE: I believe this contained something that was destined for jutil
+// https://godbolt.org/z/8D-AJb // IDK what this was, check when you feel like it
 
-#include "jinfo.hpp" 
+
+#include "jinfo.hpp"  
 
