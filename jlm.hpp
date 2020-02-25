ae54a0a6200302433d4f12639af08ae53b2e06f4
Jeffrey Cash <jtcash@ucsd.edu>
2020-02-25 04:52:23 -0800

    making generic putters for glm types

--- jlm.hpp
+++ jlm.hpp
@@ -2,21 +2,69 @@
 
 #include <iostream>
 
+#include <utility>
 
 #include <glm/glm.hpp>
 
 
 
+#include "jtype.hpp"
 
-template<class... OsTypes>
-inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec3& v) {
-  return os << '(' << (v.x) << ", " << (v.y) << ", " << (v.z) << ')';
+
+// template<class... OsTypes>
+// inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec4& v) {
+//   return os << '(' << (v.x) << ", " << (v.y) << ", " << (v.z) << ", " << (v.w) << ')';
+// }
+// template<class... OsTypes>
+// inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec3& v) {
+//   return os << '(' << (v.x) << ", " << (v.y) << ", " << (v.z) << ')';
+// }
+// template<class... OsTypes>
+// inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec2& v) {
+//   return os << '(' << (v.x) << ", " << (v.y) << ')';
+// }
+
+
+namespace jeff{
+  template<class... OsTypes, auto N, class T, auto Q, class Sep>
+  inline std::basic_ostream<OsTypes...>& glm_printer(std::basic_ostream<OsTypes...>& os, 
+            const glm::vec<N, T, Q>& v,
+            Sep&& sep) {
+    os << v[0];
+    if constexpr (N > 1)
+      for (auto n = 1; n<N; ++n)
+        os << std::forward<Sep>(sep) << v[n];
+    return os;
+  }
 }
-template<class... OsTypes>
-inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec2& v) {
-  return os << '(' << (v.x) << ", " << (v.y) << ')';
+template<class... OsTypes, auto N, class T, auto Q>
+inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::vec<N,T,Q>& v) {
+  return jeff::glm_printer(os << "< ", v, ", ") << " >";
 }
-//
+
+
+
+// M rows; N columns 
+template<class... OsTypes, auto N, auto M, class T, auto Q>
+inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>& os, const glm::mat<N, M, T, Q>& m) {
+  if constexpr (M == 1) {
+    return jeff::glm_printer(os << "[ ", glm::transpose(m)[0], " ") << " ]";
+    //return os << "[ " << m[0][0] << " ]";
+  }
+
+  return os << "TODO";/*
+
+
+
+
+
+  os << "< " << v[0];
+  if constexpr (N > 1)
+    for (auto n = 1; n<N; ++n)
+      os << ", " << v[n];
+  return os << " >";*/
+}
+////
 //namespace jeff {
 //  template<class Os>
 //  decltype(auto) os_putter(Os&& os, const glm::vec3& v) {
@@ -46,7 +94,7 @@ inline std::basic_ostream<OsTypes...>& operator<<(std::basic_ostream<OsTypes...>
 namespace jeff {
 
   template<class Toki, class Dest>
-  constexpr static void parse_vec3(Toki& toki, Dest& dest) {
+  inline constexpr void parse_vec3(Toki& toki, Dest& dest) {
     using float_type = Dest::value_type;
     auto parsed = toki.getTokensAs<float_type, float_type, float_type>();
     if (!parsed)
@@ -54,11 +102,32 @@ namespace jeff {
     dest = std::make_from_tuple<glm::vec3>(*parsed);
   }
   template<class Toki, class Dest>
-  constexpr static void parse_vec2(Toki& toki, Dest& dest) {
+  inline constexpr void parse_vec2(Toki& toki, Dest& dest) {
     using float_type = Dest::value_type;
     auto parsed = toki.getTokensAs<float_type, float_type>();
     if (!parsed)
       throw std::runtime_error("Failed to parse vec2");
     dest = std::make_from_tuple<glm::vec2>(*parsed);
   }
+  template<class Toki, class Dest>
+  inline constexpr void parse_vec4(Toki& toki, Dest& dest) {
+    using float_type = Dest::value_type;
+    auto parsed = toki.getTokensAs<float_type, float_type, float_type, float_type>();
+    if (!parsed)
+      throw std::runtime_error("Failed to parse vec4");
+    dest = std::make_from_tuple<glm::vec4>(*parsed);
+  }
+
+  template<class Toki, class Dest>
+  inline constexpr void parse_vec(Toki& toki, Dest& dest) {
+    if constexpr (jeff::is_same_nocvref_v<Dest, glm::vec2>)
+      return parse_vec2(toki, dest);
+    else if constexpr (jeff::is_same_nocvref_v<Dest, glm::vec3>)
+      return parse_vec3(toki, dest);
+    else if constexpr (jeff::is_same_nocvref_v<Dest, glm::vec4>)
+      return parse_vec4(toki, dest);
+    else
+      throw "TODO: Implement other types";
+  }
+
 }// end jeff
